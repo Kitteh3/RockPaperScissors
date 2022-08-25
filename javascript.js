7b593a92741af2514e9201984a1e975973642094
Kitteh3 <heyheymamakat@gmail.com>
2022-08-25 18:03:27 -0400

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -16,4 +16,40 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-console.log(getComputerChoice());
+
+/* Asking for user selection */
+let userChoice = prompt("Choose your weapon. Type rock, paper, or scissors.");
+
+/* One round of the game */
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection = "rock") {
+        if (computerSelection = "paper") {
+            return "Paper beats rock. You lose! Loser.";
+        } else if (computerSelection = "scissors") {
+            return "Rock beats scissors. Guess that means you win that round.";
+        } else {
+            return "It's a tie, but a winner must be named...try again.";
+        }
+    }
+    if (playerSelection = "paper") {
+        if (computerSelection = "scissors") {
+            return "My scissors cut you all up. You lose!";
+        } else if (computerSelection = "rock") {
+            return "My rock has been defeated. Damn you, human!";
+        } else {
+            return "Paper vs. paper. The battle no one asked for. Try again.";
+        }
+    }
+    if (playerSelection = "scissors") {
+        if (computerSelection = "rock") {
+            return "Ha Ha! My rock has obliterated your puny scissors! You lose.";
+        } else if (computerSelection = "paper") {
+            return "You have sliced my paper to bits. You win...but I don't like it.";
+        } else {
+            return "Scissor duel to the death! JK. It's a tie. Try again.";
+        }
+    }
+}
+const playerSelection = userChoice.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
