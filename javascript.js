7e07e40b6f3ca16afb6ef7031fc0bca897807878
Kitteh3 <heyheymamakat@gmail.com>
2022-08-26 17:09:47 -0400

    Debug loop funtion, create more problems than you started with

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
-/* Generating random number for the idiot computer */
+/* Scores */
+let playerScore = Number(0);
+let computerScore = Number(0);
+
+/* Asking for user selection */
+function getPlayerGuess() {
+    let userChoice = prompt("Choose your weapon. Type rock, paper, or scissors.");
+    return userChoice;
+}
+
+/* Telling the idiot computer how to make it's choice */
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max-min + 1) + min);
 }
-
-/* Telling the idiot computer how to use the random number to play the game */
 function getComputerChoice() {
     let randomChoice = getRandomIntInclusive(1, 3);
     if (randomChoice === 1) {
@@ -17,17 +25,8 @@ function getComputerChoice() {
     }
 }
 
-/* Win or lose? */
-function updateScores(playerScore, computerScore) {
-    if ((playRound(playerSelection, computerSelection)) === "You Lose.") {
-
-    }
-}
-
 /* One round of the game */
 function playRound(playerSelection, computerSelection) {
-    userChoice
-    console.log(playerSelection);
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             alert("Paper beats rock. You lose! Loser.")
@@ -64,47 +63,53 @@ function playRound(playerSelection, computerSelection) {
             return "Tie";
         }
     }
-    if (playerSelection !== "paper" || playerSelection !== "rock" || playerSelection !== "scissors") {
-        alert("You have to choose a weapon available. Dummy.")
-        return 0;
-    }
 }
-/* Asking for user selection */
-const userChoice = prompt("Choose your weapon. Type rock, paper, or scissors.");
-const playerSelection = userChoice.toLowerCase();
-/* Getting the computer's selection */
-const computerSelection = getComputerChoice();
-
-let playerScore = 0;
-let computerScore = 0;
-
 /* Win or lose? */
 function updateScores(playerScore, computerScore) {
     if (playRound(playerSelection, computerSelection) === "You Lose.") {
-        computerScore += 1;
+        computerScore = computerScore + 1;
     } else if (playRound(playerSelection, computerSelection) === "You Win.") {
-        playerScore += 1;
+        playerScore = playerScore + 1;
     } else if (playRound(playerSelection, computerSelection) === "Tie") {
-        computerScore += 0;
-        playerScore += 0;
-    }
+        computerScore+=0;
+        playerScore+=0;
+    }  
+    return (playerScore, computerScore);
 }
 
-/* Loop the round to play a 5 round game */
+/* Clear Selections */
+function clearSelections(playerSelection, computerSelection) {
+    return (playerSelection === 0, computerSelection === 0);
+}
 
-function game() {
+/* Getting the computer's selection */
+let computerSelection = getComputerChoice();
+/* Getting the player's selection */
+let playerSelection = getPlayerGuess();
+
+/* The game */
+function playGame() {
     for (let i=0; i<5; i++) {
+        getPlayerGuess();
+        console.log(playerSelection);
+        getComputerChoice();
+        console.log(computerSelection);
         playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
         updateScores(playerScore, computerScore);
-        if (playerScore >= 3) {
-            alert("You've won the game! Congratulations. Now you've made an enemy.");
-            return "Winner!";
-        } else if (playerScore <= 3) {
-            alert("You've lost the game. Don't cry, human. I've always been superior.");
-            return "Loser.";
-        }
+        console.log(playerScore, computerScore)
+        clearSelections(playerSelection, computerSelection);
+        console.log(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        alert("You've won the game! Congratulations. Now you've made an enemy.");
+        return "Winner!";
+    } else if (playerScore < computerScore) {
+        alert("You've lost the game. Don't cry, human. I've always been superior.");
+        return "Loser.";
+    } else {
+        alert("Something went terribly wrong.")
     }
 }
-
-/* Results */
-console.log(game());
+/* intializing game...do I need this? */
+playGame();
